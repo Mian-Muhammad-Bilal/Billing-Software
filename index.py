1895ac2e44df8f13c1c929932ebcc88f3b449aa7
Mian-Muhammad-Bilal <mian.muhammad.bilal.liaqat@gmail.com>
2024-03-04 11:14:07 +0500

    index.py Update(added price enries , totaling function and name suggestions list)

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt, QDateTime
-from PyQt5.QtWidgets import QMessageBox, QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, QPushButton, QComboBox, QCheckBox, QFrame, QWidget
+from PyQt5.QtWidgets import QMessageBox, QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, QPushButton, QComboBox, QCheckBox, QFrame, QWidget, QCompleter
 
 
 class BillingSoftware(QMainWindow):
@@ -20,9 +20,32 @@ class BillingSoftware(QMainWindow):
         self.setup_ui()
 
     def calculate_total(self):
-        # Add your logic for calculating the total amount based on selected options
-        # This function will be called when the user clicks the "Calculate Total" button
-        pass
+        try:
+            # Get prices from the additional price entry fields and convert to float
+            price_condenser = float(self.price_entry_condenser.text(
+            )) if self.price_entry_condenser.text() else 0.0
+            price_evaporator = float(self.price_entry_evaporator.text(
+            )) if self.price_entry_evaporator.text() else 0.0
+            price_filter = float(self.price_entry_filter.text(
+            )) if self.price_entry_filter.text() else 0.0
+            price_freezer = float(self.price_entry_freezer.text(
+            )) if self.price_entry_freezer.text() else 0.0
+            price_gas = float(self.price_entry_gas.text()
+                              ) if self.price_entry_gas.text() else 0.0
+            price_compressor = float(self.price_entry_compressor.text(
+            )) if self.price_entry_compressor.text() else 0.0
+
+            # Calculate total amount
+            total_amount = price_condenser + price_evaporator + \
+                price_filter + price_freezer + price_gas + price_compressor
+
+            # Update the total label
+            self.total_label.setText(f"Total Rs: {total_amount:.0f}")
+
+        except ValueError:
+            # Handle the case where the conversion to float fails (e.g., non-numeric input)
+            self.total_label.setText(
+                "Invalid input. Please enter Numbers.")
 
     def save_receipt(self):
         # Get data from the entries and other widgets
@@ -38,11 +61,37 @@ class BillingSoftware(QMainWindow):
         filter_value = self.filter_entry.text()
         evaporator_value = self.evaporator_entry.text()
 
+        # Get prices from the additional price entry fields and convert to float
+        price_compressor = float(self.price_entry_compressor.text()
+                                 ) if self.price_entry_compressor.text() else 0.0
+        price_condenser = float(self.price_entry_condenser.text(
+        )) if self.price_entry_condenser.text() else 0.0
+        price_evaporator = float(self.price_entry_evaporator.text(
+        )) if self.price_entry_evaporator.text() else 0.0
+        price_filter = float(self.price_entry_filter.text()
+                             ) if self.price_entry_filter.text() else 0.0
+        price_freezer = float(self.price_entry_freezer.text(
+        )) if self.price_entry_freezer.text() else 0.0
+        price_gas = float(self.price_entry_gas.text()
+                          ) if self.price_entry_gas.text() else 0.0
+
+        # Calculate total amount
+        total_amount = price_compressor + price_condenser + \
+            price_evaporator + price_filter + price_freezer + price_gas
+
         # Create a string with the receipt information
-        receipt_info = f"Customer Name: {customer_name}\nCustomer Phone: {customer_phone}\nCustomer Address: {customer_address}\n" \
-            f"Compressor: {compressor_value}\nModel No: {model_value}\nGas Charge: {gas_value}\n" \
-            f"Freezer Work: {freezer_value}\nCondenser: {condenser_value}\n" \
-            f"Filter: {filter_value}\nEvaporator: {evaporator_value}"
+        receipt_info = f"Customer Details\n"\
+            f"Customer Name: {customer_name}\nCustomer Phone: {customer_phone}\nCustomer Address: {customer_address}\n" \
+            f"---------------------------------------\n"\
+            f"Bill:\n"\
+            f"Compressor: {compressor_value}- Model No: {model_value} - Price: {price_compressor}\n" \
+            f"Gas Charge: {gas_value} - Price: {price_gas}\n" \
+            f"Freezer Work: {freezer_value} - Price: {price_freezer}\n" \
+            f"Condenser: {condenser_value} - Price: {price_condenser}\n" \
+            f"Filter: {filter_value} - Price: {price_filter}\n" \
+            f"Evaporator: {evaporator_value} - Price: {price_evaporator}\n" \
+            f"---------------------------------------\n"\
+            f"Total Price: {total_amount:.2f}"
 
         # Save the receipt to a file with a timestamp in the filename
         timestamp = QDateTime.currentDateTime().toString("dd_MM_yyyy-hh:mm:ss")
@@ -56,6 +105,21 @@ class BillingSoftware(QMainWindow):
         except Exception as e:
             QMessageBox.warning(
                 self, "Save Error", f"Error occurred while saving receipt: {str(e)}")
+
+    def setup_autosuggestion(self, line_edit, suggestions):
+        completer = QCompleter(suggestions)
+        completer.setFilterMode(Qt.MatchContains)
+        line_edit.setCompleter(completer)
+
+    def load_suggestions_from_file(self, filename):
+        try:
+            with open(filename, 'r') as file:
+                suggestions = file.read().splitlines()
+                return suggestions
+        except Exception as e:
+            QMessageBox.warning(
+                self, "Error", f"Error reading suggestions from file: {str(e)}")
+            return []
 
     def setup_ui(self):
         layout = QVBoxLayout()
@@ -88,6 +152,10 @@ class BillingSoftware(QMainWindow):
         customer_name_layout = QHBoxLayout()
         customer_name_label = QLabel("Name:", self)
         self.customer_name_entry = QLineEdit(self)
+
+        # Customer Name Auto-suggestion from File
+        names_suggestions = self.load_suggestions_from_file("names.txt")
+        self.setup_autosuggestion(self.customer_name_entry, names_suggestions)
 
         customer_name_layout.addWidget(customer_name_label)
         customer_name_layout.addWidget(self.customer_name_entry)
@@ -130,7 +198,7 @@ class BillingSoftware(QMainWindow):
         self.model_entry = QLineEdit(self)
 
         price_label_model = QLabel("Compressor Price:", self)
-        self.price_entry_model = QLineEdit(self)
+        self.price_entry_compressor = QLineEdit(self)
 
         self.compressor_combo.addItems(compressor_options)
         compressor_layout.addWidget(compressor_label)
@@ -138,7 +206,7 @@ class BillingSoftware(QMainWindow):
         compressor_layout.addWidget(model_label)
         compressor_layout.addWidget(self.model_entry)
         compressor_layout.addWidget(price_label_model)
-        compressor_layout.addWidget(self.price_entry_model)
+        compressor_layout.addWidget(self.price_entry_compressor)
         self.central_layout.addLayout(compressor_layout)
 
         # Gas Charge Section with Price Entry
@@ -214,8 +282,14 @@ class BillingSoftware(QMainWindow):
         evaporator_layout.addWidget(self.evaporator_entry)
         evaporator_layout.addWidget(price_label_evaporator)
         evaporator_layout.addWidget(self.price_entry_evaporator)
-        # Button to Calculate Total
         self.central_layout.addLayout(evaporator_layout)
+
+        # Total label
+        self.total_label = QLabel("Total Rs: 0", self)
+        self.central_layout.addWidget(
+            self.total_label, alignment=Qt.AlignCenter)
+
+        # Button to Calculate Total
         calculate_button = QPushButton("Calculate Total", self)
         calculate_button.setStyleSheet(
             "background-color: #4CAF50; color: white;")
